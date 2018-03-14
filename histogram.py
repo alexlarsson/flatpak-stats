58bd447bce36c5e09dbf38fe1882f71026645d97
Alexander Larsson <alexl@redhat.com>
2018-03-14 16:32:29 +0100

    Update histogram to use filtered logs

--- histogram.py
+++ histogram.py
@@ -1,26 +1,59 @@
 #!/usr/bin/env python3
 
+import argparse
+import time
 import stats
 import sys
 import pickle
+import datetime
+import gzip
 
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-else:
-    filename = "refs.db"
-refs_db = open(filename, "rb")
-refs = pickle.load(refs_db)
+parser = argparse.ArgumentParser(description='Print a histograme.')
+parser.add_argument('--from-date', metavar='DATE')
+parser.add_argument('--to-date', metavar='DATE')
+parser.add_argument('file', nargs='*')
+args = parser.parse_args()
 
-all = []
-for ref in refs:
-    if ref.find(".Locale/") != -1 or ref.startswith("appstream/"):
-        continue
-    accesses = refs[ref];
-    unique = {}
-    for a in accesses:
-        ip = a[3][0]
-        unique[ip] = True
-    all.append( (len(accesses), len(unique), ref) )
+from_date = None
+if args.from_date:
+    from_date = time.mktime(time.strptime(args.from_date, '%d/%m/%Y'))
+to_date = None
+if args.to_date:
+    to_date = time.mktime(time.strptime(args.to_date, '%d/%m/%Y')) + 24 * 60 * 60
 
-for count, unique, ref in sorted(all, reverse=True):
-    print("{}\t{}\t{}".format (count, unique, ref))
+refs = {}
+
+def parse_log(logname):
+    if logname.endswith(".gz"):
+        log_file = gzip.open(logname, 'rb')
+    else:
+        log_file = open(logname, 'r')
+
+    while True:
+        line = log_file.readline().decode("utf8")
+        if line == "":
+            break
+
+        l = line.split("\t")
+        ref = l[2]
+
+        if ref.find(".Locale/") != -1:
+            continue
+
+        if from_date or to_date:
+            date = time.mktime(time.strptime(l[1], '%d/%b/%Y:%H:%M:%S %z'))
+            if from_date and date < from_date:
+                continue
+            if to_date and date > to_date:
+                continue
+
+        if ref in refs:
+            refs[ref] = refs[ref] + 1
+        else:
+            refs[ref] = 0
+
+for logname in args.file:
+    parse_log(logname)
+
+for ref, count in sorted(refs.items(), reverse=True, key=lambda tuple: tuple[1]):
+    print("{}\t{}".format (count, ref))
